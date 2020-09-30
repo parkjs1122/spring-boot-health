8ab68a80474be83c30d2f6a821da12c879208297
parkjs1122 <32847780+parkjs1122@users.noreply.github.com>
2020-09-30 12:46:01 +0900

    20/09/30

--- health/src/main/java/com/jspark/health/dao/GymRepository.java
+++ health/src/main/java/com/jspark/health/dao/GymRepository.java
@@ -2,7 +2,7 @@ package com.jspark.health.dao;
 
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Point;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jspark.health.dto.Gym;
@@ -10,7 +10,7 @@ import com.jspark.health.dto.Gym;
 import reactor.core.publisher.Flux;
 
 @Repository
-public interface GymRepository extends MongoRepository<Gym, String> {
+public interface GymRepository extends ReactiveCrudRepository<Gym, String> {
 	public Flux<Gym> findByNameIgnoreCaseLikeAndLocationNear(String name, Point location, Distance distance);
 	public Flux<Gym> findByAddress(String address);
 	public Flux<Gym> findByLocationNear(Point location, Distance distance);
